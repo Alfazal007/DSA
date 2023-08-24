29c98056fdbad6f5bb21404b2be00e4ac214f51b
Alfazal007 <alfazal7777@gmail.com>
2023-08-24 22:25:36 +0530

    Selection sort added

---
+++ selectionSort.java
@@ -0,0 +1,29 @@
+public class selectionSort {
+    public static void printArr(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void sort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        printArr(arr);
+        sort(arr);
+        printArr(arr);
+    }
+}
