fe15907558e00b36aa9834ff863d3f8c4d48b0b5
Alfazal007 <alfazal7777@gmail.com>
2023-08-26 13:00:16 +0530

    vowel counter in string

---
+++ vowelCounter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class vowelCounter {
+    public static void vowelCount(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+                    || str.charAt(i) == 'u') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+        vowelCount(str);
+    }
+}
