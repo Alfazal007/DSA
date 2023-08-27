7f44b9b196c64deb97c9da383df0c56c923da079
Alfazal007 <alfazal7777@gmail.com>
2023-08-27 13:12:16 +0530

    even or odd using bit operator added

---
+++ evenOrOdd.java
@@ -0,0 +1,7 @@
+public class evenOrOdd {
+    public static void main(String[] args) {
+        // to check even or add & with 1 if res is 0 then even else odd
+        System.out.println(2 & 1);
+        System.out.println(5 & 1);
+    }
+}
