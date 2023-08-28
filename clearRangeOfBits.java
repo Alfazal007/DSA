2da753ac55b450c9b4bf5ebf44bccba2707172d6
Alfazal007 <alfazal7777@gmail.com>
2023-08-29 00:37:25 +0530

    clear range of bits added

---
+++ clearRangeOfBits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class clearRangeOfBits {
+    public static int clearRangeBits(int n, int i, int j) {
+        // to create bit mask
+        int b1 = (-1) << i + 1;
+        int b2 = 1 << j - 1;
+        int bitmask = b1 | b2;
+        return bitmask & n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the value of i: ");
+        int i = sc.nextInt();
+        System.out.println("Enter the value of j: ");
+        int j = sc.nextInt();
+        int res = clearRangeBits(n, i, j);
+        System.out.println(res);
+    }
+}
