81f948d82ded154447d13d045d451b362353cab7
Alfazal007 <alfazal7777@gmail.com>
2023-08-30 22:13:02 +0530

    swap 2 numbers without temp variable

---
+++ swapNumbers.java
@@ -0,0 +1,11 @@
+public class swapNumbers {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
