7d5b10d7f2e809f5fd07bd86b83347960d58d437
Alfazal007 <alfazal7777@gmail.com>
2023-09-15 22:22:58 +0530

    queues started

---
+++ queue.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class queue {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        System.out.println(queue);
+        queue.remove();
+        System.out.println(queue);
+    }
+}
