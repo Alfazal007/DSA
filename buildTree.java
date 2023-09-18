a9e4ff8b567319284b92eeb72cb533c0ed3f9ac8
Alfazal007 <alfazal7777@gmail.com>
2023-09-18 20:02:37 +0530

    added tree traversals

---
+++ buildTree.java
@@ -0,0 +1,66 @@
+public class buildTree {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree {
+        int idx = -1;
+
+        public Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node temp = new Node(nodes[idx]);
+            temp.left = buildTree(nodes);
+            temp.right = buildTree(nodes);
+            return temp;
+        }
+
+        public static void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        public static void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        public static void postorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        int arr[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = binaryTree.buildTree(arr);
+        binaryTree.preorder(root);
+        System.out.println();
+        binaryTree.inorder(root);
+        System.out.println();
+        binaryTree.postorder(root);
+    }
+}
